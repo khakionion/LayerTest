d449dfd83f3d48bbbc88824506752603168d3148
Michael Herring <webmaster@khakionion.com>
2014-09-04 08:58:33 +0900

    initial checkin

---
+++ Assets/SpacebarAction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpacebarAction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Animator anim = GetComponent<Animator>();
+
+		if(Input.GetKeyDown("space")) {
+			anim.SetTrigger("PerformAction");
+		}
+	}
+}
